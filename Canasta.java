88e0b6fcb403893fce0bf43bc32a8b53307d28b8
ricardo <ricardocruz.a89@gmail.com>
2011-02-22 14:01:42 -0600

    clase canasta y pelota

---
+++ Canasta.java
@@ -0,0 +1,9 @@
+import java.awt.Image;
+
+public class Canasta extends Objeto {
+	
+	public Canasta(int posX, int posY, Image image) {
+		
+		super(posX, posY, image);
+	}
+}
---
+++ Pelota.java
@@ -0,0 +1,9 @@
+import java.awt.Image;
+
+public class Pelota extends Objeto {
+	
+	public Pelota(int posX, int posY, Image image) {
+		
+		super(posX, posY, image);
+	}
+}
