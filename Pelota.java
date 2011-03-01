a4daff0a9d3b1470aa652dc35ac6c25475193567
ricardo <ricardocruz.a89@gmail.com>
2011-03-01 00:38:38 -0600

    rectangulo de pelota modificado

--- Pelota.java
+++ Pelota.java
@@ -1,9 +1,15 @@
 import java.awt.Image;
+import java.awt.Rectangle;
 
 public class Pelota extends Objeto {
 	
 	public Pelota(int posX, int posY, Image image) {
 		
 		super(posX, posY, image);
+	}
+	
+	public Rectangle getPerimetro() { 
+		
+		return new Rectangle(getPosX() + 20, getPosY() + 24, 23, 23);
 	}
 }
